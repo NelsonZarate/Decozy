344b5ee8c7fe8290bc8b698100f4487d14efef02
NelsonZarate <nelson_zarate@eticalgarve.com>
2026-06-11 20:27:35 +0000

    feat: added upload endpoint, fix frontend nginx structure

--- backend/src/app/core/settings.py
+++ backend/src/app/core/settings.py
@@ -4,22 +4,28 @@ from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    secret_key: str
-    algorithm: str
-    access_token_expire_minutes: int
-
-    database_driver: str
-    database_username: str
+# --- Variáveis da Base de Dados (Ajustadas para bater com o teu .env) ---
+    database_driver: str = "postgresql+psycopg2"
+    database_username: str  # 👈 Mudado de 'database_user' para 'database_username'
     database_password: str
-    database_host: str
-    database_port: int
+    database_host: str = "database"
+    database_port: int = 5432
     database_name: str
 
-    environment: str
-    port: int
-    
-    class Config:
-        env_file = ".env"
+    # --- Variáveis com Valores Padrão ---
+    secret_key: str = "super_secret_key_de_desenvolvimento_123"
+    algorithm: str = "HS256"
+    access_token_expire_minutes: int = 30
+    environment: str = "development"
+    port: int = 8000
+    upload_dir: str = "static/uploads"
+
+    model_config = {
+        "env_file": ".env",
+        "env_file_encoding": "utf-8",
+        "extra": "ignore",
+        "case_sensitive": False  # 👈 ISTO FAZ O PYDANTIC ACEITAR TANTO 'UPLOAD_DIR' COMO 'upload_dir'!
+    }
 
 @lru_cache
 def get_settings():
