2ebacef5d71eb5810272fd6c58b72f77ec499c70
tomas4030 <tomas_miguel@eticalgarve.com>
2026-06-16 20:42:12 +0000

    feat: add user router with endpoints for managing saved items and user info

--- backend/src/app/main.py
+++ backend/src/app/main.py
@@ -3,15 +3,26 @@ import os
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 
-# Importar e incluir os routers
 from app.api.v1.api import api_router
 from app.core.settings import settings
-
+from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI(title="Decozy API")
 
-# 🛠️ Configurar a pasta de ficheiros estáticos
 os.makedirs(settings.upload_dir, exist_ok=True)
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
+
+origins = [
+    "http://localhost:3000",    
+    "http://127.0.0.1:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,            # Permite pedidos apenas destas origens
+    allow_credentials=True,           # Permite cookies e headers de autenticação (essencial para o OAuth/JWT)
+    allow_methods=["*"],              # Permite todos os métodos (GET, POST, PUT, DELETE, etc.)
+    allow_headers=["*"],              # Permite todos os cabeçalhos (Headers)
+)
 
 app.include_router(api_router, prefix="/api/v1")
